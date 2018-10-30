7da7bbd992fa92956f87a21e42a88e070c54a69b
fmccastro <franciscomccastro@hotmail.com>
2018-10-30 00:15:26 +0000

    with area scoring

--- project1/go.py
+++ project1/go.py
@@ -1,6 +1,64 @@
+infinity = 999999999
+
+def alphabeta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None): 
+    """Search game to determine best action; use alpha-beta pruning.
+    This version cuts off search and uses an evaluation function."""
+
+    player = game.to_move(state)
+
+    # Functions used by alphabeta
+    def max_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = -infinity
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a),
+                                 alpha, beta, depth + 1))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+    
+    def min_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = infinity
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a),
+                                 alpha, beta, depth + 1))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alphabeta_cutoff_search starts here:
+    # The default test cuts off at depth d or at a terminal state
+    cutoff_test = (cutoff_test or
+                   (lambda state, depth: depth > d or
+                    game.terminal_test(state)))
+    eval_fn = eval_fn or (lambda state: game.utility(state, player))
+    best_score = -infinity
+    beta = infinity
+    best_action = None
+    for a in game.actions(state):
+        v = min_value(game.result(state, a), best_score, beta, 1)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
 
 class Game():
 
+    def __init__(self, board_file):
+
+        try:
+            with open(board_file, 'r+') as file:
+                
+                self.state = self.load_board(file)
+
+        except Exception as e:
+            print('__init__: {}'.format(e))
+            return
 
     def to_move(self, s):
 
@@ -16,15 +74,14 @@ class Game():
         Returns a boolean of whether state s is terminal
         """
 
-
         def in_dict(elem, dictionary):   # checks if a given coordinate [a, b] is already cointained in the dictionary of strings ..input..elem = [a, b] 
             for key, value in dictionary.items():
                 for array in value:
                     if elem == array:
-                        return [1, key]
-            return [0, 0]
+                        return [1, key]   # contained
+            return [0, 0] # not-contained
             
-        def strings(state):   # builds a dictionary of the strings at a given game state
+        def strings(state):   # builds a dictionary of strings at a given game state
             
             count = 0
             all_strings = {}
@@ -84,7 +141,28 @@ class Game():
             return 0
                         
         else:
-            return 2
+            return self.area_scoring(s)
+    
+    def area_scoring(self, s):  #só conta as peças de cada jogador por enquanto
+
+        whites_points = 0
+        blacks_points = 0
+        board_size = len(s["board"])
+
+        for i in range(0, board_size):  
+                for j in range(0, board_size):
+                    if s["board"][i][j] == str(1):
+                        blacks_points+=1
+                    elif s["board"][i][j] == str(2):
+                        whites_points+=1
+        
+        
+    
+        if s["next_player"] == 1:
+            return blacks_points - whites_points
+        
+        elif s["next_player"] == 2:
+            return whites_points - blacks_points
             
     def actions(self, s): 
 
@@ -116,7 +194,6 @@ class Game():
             j = 1
             i+=1
 
-            
         return actions
 
     def adjacents(self, a, b, state):
@@ -157,8 +234,6 @@ class Game():
             positions.extend([[a - 1, b], [a + 1, b], [a, b - 1], [a, b + 1]])
             return positions
 
-
-
     def copy_board(self, board):
 
         """
@@ -172,8 +247,6 @@ class Game():
                 new_board[-1].append(point)
 
         return new_board
-
-
 
     def result(self, s, a):
 
@@ -209,7 +282,7 @@ class Game():
         0210
         0000
         """
-
+        
         current_state = {}
 
         try:
@@ -233,4 +306,23 @@ class Game():
             print('ERROR - load_board: {}'.format(e))
 
         return current_state
+################################################################################################################
 
+finish = 0
+action = ()
+
+atari_go = Game("board.txt")
+print(atari_go.state)
+
+while True:
+    finish = atari_go.utility(atari_go.state, atari_go.to_move(atari_go.state))
+    print("Finish ", finish)
+
+    if finish == 1 or finish == -1 or finish == 0:
+        break
+    
+    action = alphabeta_cutoff_search(atari_go.state, atari_go, eval_fn = atari_go.terminal_test)
+    print("Action ", action)
+
+    atari_go.state = atari_go.result(atari_go.state, action)
+    print("State ", atari_go.state)
