a281afb42951f32e7f9b0fe037c77e0a7129c352
rffc14 <rffc.14@gmail.com>
2018-11-03 00:46:26 +0000

    test

--- project1/moodle.py
+++ project1/moodle.py
@@ -166,16 +166,13 @@ class Game():
         for i in range(0, board_size):
             for j in range(0, board_size):
                 if s["board"][i][j] == 0:
-                    for (coord_1, coord_2) in self.adjacents(i, j, board_size):
-                        if s["board"][coord_1][coord_2] == 0:
-                            actions.append((p, i+1, j+1))
-                            break
+                    if 0 in self.adjacents2(i,j,s["board"]):
+                        actions.append((p, i+1, j+1))
+                    else:
                         winner = self.winner(self.result(s, (p, i+1,j+1)))
                         if winner >= 0:
                             if winner == p or winner == 0:
                                 actions.append((p, i+1, j+1))
-                    #else:
-                        #actions.append((p, i+1, j+1))
         return actions
         
     def adjacents(self, a, b, N):
