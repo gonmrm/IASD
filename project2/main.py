d9a7f3218e9560df4d81d75c1446629fdb769cce
rffc14 <rffc.14@gmail.com>
2018-12-06 22:04:26 +0000

    7/9

--- project2/main.py
+++ project2/main.py
@@ -1,16 +1,17 @@
 import csp
 
 class Problem(csp.CSP):
-    
+    upper_bound=0
     def __init__(self, fh):
-    #   Place here your code to load problem from opened file object fh and
-    #   set variables, domains, graph, and constraint_function accordingly
-
-        #fd = open('input.txt','r')
-        self.variables=[]
-        T=[]
-        A=[]
-        R=[]
+    
+        variables=[]
+        domain=dict()
+        neighbors={}
+        blocks=[]
+        constrA=[]
+        rooms=[]
+        solution=dict()
+        
         for line in fh:
             key = line[0]
             element_list = []
@@ -27,29 +28,27 @@ class Problem(csp.CSP):
                         element = element[:-1]
                     element_list.append(element)
             if key=='T':
-                T = list(element_list)
+                for var in element_list:
+                    blocks.append(','.join(var))
             elif key=='S':
                 S = list(element_list)
             elif key=='R':
-                R = list(element_list)
+                rooms = list(element_list)
             elif key=='W':
                 for var in element_list:
-                    self.variables.append(','.join(var))
+                    variables.append(','.join(var))
             else:
-                A = list(element_list)
-        domain=dict()
-        neighbors={}
-        for key in self.variables:
+                constrA = list(element_list)
+        
+        
+        for key in variables:
             lis=[]
-            for block in T:
-                for room in R:
-                    block.append(room)
-                    copy = list(block)
-                    lis.append(copy)
-                    block.pop()
+            for block in blocks:
+                for room in rooms:
+                    lis.append(block+','+room)
             domain[key] = lis
             n_list=[]
-            for key2 in self.variables:
+            for key2 in variables:
                 if key==key2:
                     continue
                 n_list.append(key2)
@@ -62,21 +61,25 @@ class Problem(csp.CSP):
             for turnos in temp['A']:
         '''
 
-        def constraints_function(self, A, a, B, b):
+        def constraints_function(A, a, B, b):
 
             A=A.split(',')
             B=B.split(',')
-            
-            if A[0:2]==B[0:2] and a[0]==b[0]:  # Duas aulas do mesmo tipo no mesmo dia, não pode ser
+            a=a.split(',')
+            b=b.split(',')
+
+            if int(a[1])>self.upper_bound or int(b[1])>self.upper_bound:
+                return False
+            elif A[0:2]==B[0:2] and a[0]==b[0]:  # Duas aulas do mesmo tipo no mesmo dia, não pode ser
                 return False
             elif a[0:2]==b[0:2]:               # Duas aulas no mesmo dia à mesma hora
                 if a[2]==b[2]:                 # Caso sejam na mesma sala, não pode ser
                     return False
                 turnos=[]
-                for conjunto in self.constr['A']: # Verificar se há turnos em comum nas duas aulas, caso haja não pode ser, há sobreposição
+                for conjunto in constrA: # Verificar se há turnos em comum nas duas aulas, caso haja não pode ser, há sobreposição
                     if conjunto[1]==A[0]:
                         turnos.append(conjunto[0])    # Reunir turnos com disciplina A e verificar se existem na disciplina B
-                for conjunto in self.constr['A']:
+                for conjunto in constrA:
                     if conjunto[1]==B[0]:
                         if conjunto[0] in turnos:     # Verificar se turnos com a disciplina têm também a disciplina B
                             return False
@@ -87,12 +90,14 @@ class Problem(csp.CSP):
                     if turno in A[0] and turno in B[0]: # A[0]='IASD' B[0]='SCDTR'
                         return False
                 '''
-        super().__init__(self.variables, domain, neighbors, constraints_function)
+
+        super().__init__(variables, domain, neighbors, constraints_function)
         
     def dump_solution(self, fh):
         #  Place here your code to write solution to opened file object fh
-        print(1)
-        
+        for key in self.solution:
+            fh.write(key+' '+','.join(self.solution[key].split(',')[0:2])+' '+self.solution[key].split(',')[2]+'\n')
+        return
     def cost_function(self, output_file):
         print(1)#for line in fh: # See solution and find latest class percorrer linhas e procurar 
         cost=0
@@ -103,10 +108,32 @@ class Problem(csp.CSP):
 
 def solve(input_file, output_file):
     p=Problem(input_file)
-    solution=csp.backtracking_search(p)
-    print(solution)
-    #print(1)
-    #p.dump_solution(output_file)
+    p.upper_bound=10
+    optimal=False
+    down=True
+    up=False
+    p.solution=csp.backtracking_search(p)
+    if p.solution!=None: # Solution exists must decrease bound
+        p.upper_bound-=1
+        down=True
+    else:               # Solution does not exist must increase bound
+        p.upper_bound+=1
+        up=True
+    while True:
+        p.solution=csp.backtracking_search(p)
+        if p.solution==None:
+            if down==True:
+                p.upper_bound+=1
+                p.solution=csp.backtracking_search(p)
+                break
+            elif up==True:
+                p.upper_bound+=1
+        elif p.solution!=None:
+            if down==True:
+                p.upper_bound-=1
+            elif up==True:
+                break
+    p.dump_solution(output_file)
 
 
     
