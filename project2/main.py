a7de47c7d82435e2383e53970d31465d640cc526
rffc14 <rffc.14@gmail.com>
2018-12-07 02:14:21 +0000

    8/9

--- project2/main.py
+++ project2/main.py
@@ -68,28 +68,30 @@ class Problem(csp.CSP):
             a=a.split(',')
             b=b.split(',')
 
-            if A[0:2]==B[0:2] and a[0]==b[0]:  # Duas aulas do mesmo tipo no mesmo dia, não pode ser
+            if (int(a[1]) > self.upper_bound):
                 return False
-            elif a[0:2]==b[0:2]:               # Duas aulas no mesmo dia à mesma hora
-                if a[2]==b[2]:                 # Caso sejam na mesma sala, não pode ser
-                    return False
+            if (int(b[1]) > self.upper_bound):
+                return False
+            if A[0:2] == B[0:2] and a[0] == b[0]:   # Duas aulas do mesmo tipo no mesmo dia, não pode ser
+                return False
+            if a[0:3] == b[0:3]:                   # Duas aulas no mesmo dia à mesma hora mesma sala
+                return False
+            if a[0:2]==b[0:2]:
                 turnos=[]
-                for conjunto in constrA: # Verificar se há turnos em comum nas duas aulas, caso haja não pode ser, há sobreposição
-                    if conjunto[1]==A[0]:
-                        turnos.append(conjunto[0])    # Reunir turnos com disciplina A, depois verificar turnos com disciplina B
+                for conjunto in constrA:            # Verificar se há turnos em comum nas duas aulas, caso haja não pode ser, há sobreposição
+                    if conjunto[1] == A[0]:
+                        turnos.append(conjunto[0])  # Reunir turnos com disciplina A, depois verificar turnos com disciplina B
                 for conjunto in constrA:
-                    if conjunto[1]==B[0]:
-                        if conjunto[0] in turnos:     # Verificar se turnos com a disciplina têm também a disciplina B
+                    if conjunto[1] == B[0]:
+                        if conjunto[0] in turnos:   # Verificar se turnos com a disciplina têm também a disciplina B
                             return False
-            elif int(a[1])>self.upper_bound or int(b[1])>self.upper_bound:
-                return False
-            else:
-                return True
-                '''Pode compensar fazer um dicionario na init na forma {IASD:['turno1','turno2']} para poder fazer o que está abaixo
-                for turno in S:
-                    if turno in A[0] and turno in B[0]: # A[0]='IASD' B[0]='SCDTR'
-                        return False
-                '''
+            return True
+
+            '''Pode compensar fazer um dicionario na init na forma {IASD:['turno1','turno2']} para poder fazer o que está abaixo
+            for turno in S:
+                if turno in A[0] and turno in B[0]: # A[0]='IASD' B[0]='SCDTR'
+                    return False
+            '''
 
         super().__init__(variables, domain, neighbors, constraints_function)
         
@@ -109,29 +111,37 @@ class Problem(csp.CSP):
 def solve(input_file, output_file):
     p=Problem(input_file)
     p.upper_bound=10
-    down=True
+    down=False
     up=False
-    p.solution=csp.backtracking_search(p)
+
+    p.solution=csp.backtracking_search(p,csp.mrv,csp.lcv)
+    print(p.solution)
     if p.solution!=None: # Solution exists must decrease bound
-        p.upper_bound-=1
         down=True
     else:               # Solution does not exist must increase bound
-        p.upper_bound+=1
         up=True
+
     while True:
-        p.solution=csp.backtracking_search(p)
-        if p.solution==None:
-            if down==True:
+        
+        if down:
+            p.upper_bound-=1
+            p.solution=csp.backtracking_search(p,csp.mrv,csp.lcv)
+            if p.solution!=None:
+                pass
+            elif p.solution==None:
                 p.upper_bound+=1
-                p.solution=csp.backtracking_search(p)
+                p.solution=csp.backtracking_search(p,csp.mrv,csp.lcv)
                 break
-            elif up==True:
-                p.upper_bound+=1
-        elif p.solution!=None:
-            if down==True:
-                p.upper_bound-=1
-            elif up==True:
+        elif up:
+            p.upper_bound+=1
+            p.solution=csp.backtracking_search(p,csp.mrv,csp.lcv)
+            if p.solution==None:
+                pass
+            else:
                 break
+        
+    
+    print(p.solution)
     p.dump_solution(output_file)
 
 
@@ -145,5 +155,4 @@ This class describes finite-domain Constraint Satisfaction Problems.
                     the other variables that participate in constraints.
         constraints A function f(A, a, B, b) that returns true if neighbors
                     A, B satisfy the constraint when they have values A=a, B=b
-""" 
-
+"""
